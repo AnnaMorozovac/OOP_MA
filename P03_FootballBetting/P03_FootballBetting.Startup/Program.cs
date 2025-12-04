ac10c0c8f0d86633957a749994d6a78c2b0b150b
AnnaMorozovac <morozova.anna@chnu.edu.ua>
2025-12-04 10:10:45 +0200

    laba18_part2+

---
+++ P03_FootballBetting/P03_FootballBetting.Startup/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P03_FootballBetting.Startup
+{
+    using P03_FootballBetting.Data;
+    using P03_FootballBetting.Models;
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new FootballBettingContext())
+            {
+                context.Database.EnsureCreated();
+
+                Console.WriteLine("Done!");
+            }
+        }
+
+    }
+}
